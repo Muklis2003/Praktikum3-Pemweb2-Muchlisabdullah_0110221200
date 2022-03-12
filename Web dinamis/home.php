17775c4230a1d021eb19565f510ec50f5dd6b73b
Muklis2003 <91894493+Muklis2003@users.noreply.github.com>
2022-03-13 00:03:18 +0700

    Add files via upload

---
+++ Web dinamis/home.php
@@ -0,0 +1,15 @@
+
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>no</th>
+                    <th>nama</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Muchlis Abdullah</td>
+                </tr>
+            </tbody>
+        </table>
